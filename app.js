d9ce491bd9b44af164b57e60f5560b92d91c931c
Pablo Vera <jahirvera400@gmail.com>
2025-03-12 02:48:47 -0600

    Mostrando amigos en pantalla

--- app.js
+++ app.js
@@ -2,3 +2,30 @@
 
 
 let amigos = []
+
+function agregarAmigo() {
+    let nombrePersona = document.getElementById("amigo").value;
+
+    if (!nombrePersona) {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nombrePersona)
+        limpiarCaja();
+        recorrerArray();
+    }
+}
+
+function recorrerArray(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let persona = amigos[i];
+        let item = document.createElement("li");
+        item.textContent = persona;
+        lista.appendChild(item);
+      }
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}

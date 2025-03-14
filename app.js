17524f6b4185feeb0c0d9041570c0bf4c90752f2
Pablo Vera <jahirvera400@gmail.com>
2025-03-13 22:41:40 -0600

    Proyecto funcionando.

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
-
+// Se declara la lista donde se almacenaran los amigos.
 let amigos = []
 
+// Funcion para agregar amigo a la lista.
 function agregarAmigo() {
-    let nombrePersona = document.getElementById("amigo").value;
+    // Captura el nombre del amigo.
+    let nombreAmigo = document.getElementById("amigo").value;
 
-    if (!nombrePersona) {
+    // Verifica que el cuadro no este vacio.
+    if (nombreAmigo.trim() === "") {
         alert("Por favor, inserte un nombre.")
     } else {
-        amigos.push(nombrePersona)
-        limpiarCaja();
-        recorrerArray();
+        // Agrega el amigo a la lista.
+        amigos.push(nombreAmigo)
+        // Limpia el nombre.
+        document.querySelector('#amigo').value = '';
+        // Llama la funcion amigo para mostrarlo.
+        mostrarAmigo();
     }
 }
 
-function recorrerArray(){
+// Funcion para mostrar el amigo en la lista.
+function mostrarAmigo(){
+    // Obtiene el elemento de la lista.
     let lista = document.getElementById("listaAmigos");
+    // Limpia la lista existente.
     lista.innerHTML = "";
+    // Itera para crear el elemento y agregar el nombre.
     for (let i = 0; i < amigos.length; i++) {
-        let persona = amigos[i];
+        let amigo = amigos[i];
         let item = document.createElement("li");
-        item.textContent = persona;
+        item.textContent = amigo;
         lista.appendChild(item);
       }
 }
 
-function limpiarCaja(){
-    document.querySelector('#amigo').value = '';
+// Funcion para sortear el amigo.
+function sortearAmigo() {
+    // Verifica que el array no este vacio.
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista.")
+    } else {
+        // Genera un indice (amigo) aleatorio.
+        let indice = Math.floor(Math.random() * amigos.length);
+        let resultadoSorteo = document.getElementById("resultado")
+        // Actualiza al amigo sorteado.
+        resultadoSorteo.innerHTML = amigos[indice]
+    }
 }
